ee3518be2533faf88de87c7b75901d19e20de9f3
ericstober <erictstober@gmail.com>
2025-12-23 09:27:39 -0600

    add CartContext

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useState, useContext } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  return <CartContext.Provider value={{ cart }}>{children}</CartContext.Provider>;
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
